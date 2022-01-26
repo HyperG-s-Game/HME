13862b1b16df797195a1d9a86116e6074d7b2f02
Abhishek sinha <73241755+Laughing-wolf-gamer@users.noreply.github.com>
2022-01-26 12:55:18 +0530

    Update 19

--- Hold My Egg/Assets/_Assets/Scripts/Ad Scripts/Untiy Ads/New/AdController.cs
+++ Hold My Egg/Assets/_Assets/Scripts/Ad Scripts/Untiy Ads/New/AdController.cs
@@ -3,8 +3,10 @@ using UnityEngine;
 using GoogleMobileAds.Api;
 using WolfGamer.Hold_My_Eggs;
 using WolfGamer.Utils;
+using GameAnalyticsSDK;
 
-public class AdController : MonoBehaviour{
+public class AdController : MonoBehaviour, IGameAnalyticsATTListener
+{
     public static AdController instance;
 
     private readonly string bannerId = "ca-app-pub-1447736674902262/9281669086";
@@ -21,7 +23,8 @@ public class AdController : MonoBehaviour{
     private bool isInitialized = false;
     public static bool levelRewardAd = false;
     public static bool extraEgg = false;
-    
+    private string currentRewardedVideoPlacement;
+
     private void Awake(){
         DontDestroyOnLoad(this);
         if (instance == null){
@@ -70,19 +73,15 @@ public class AdController : MonoBehaviour{
 
             InitializeAd();
         });
-        
+
     }
 
     private void InitializeAd(){
-        interstitialAd?.Destroy();
-        interstitialAd = new InterstitialAd(interstitialId);
-
-        interstitialAd.OnAdClosed += (sender, args) => RequestInterstitial();
-
         RequestInterstitial();
 
 
         rewardedAd = new RewardedAd(rewardedId);
+        GameAnalyticsILRD.SubscribeAdMobImpressions(rewardedId, rewardedAd);
         rewardedAd.OnAdLoaded += HandleRewardedAdLoaded;
         rewardedAd.OnAdFailedToLoad += HandleRewardedAdFailedToLoad;
         rewardedAd.OnAdFailedToShow += HandleRewardedAdFailedToShow;
@@ -91,6 +90,13 @@ public class AdController : MonoBehaviour{
         rewardedAd.OnAdClosed += HandleRewardedAdClosed;
         RequestRewardedAd();
         isInitialized = true;
+
+        if (Application.platform == RuntimePlatform.IPhonePlayer){
+            GameAnalytics.RequestTrackingAuthorization(this);
+        }else{
+            GameAnalytics.Initialize();
+        }
+
     }
 
     private AdRequest RequestAd(){
@@ -102,7 +108,31 @@ public class AdController : MonoBehaviour{
     }
 
     private void RequestInterstitial(){
-        interstitialAd.LoadAd(RequestAd());
+#if UNITY_ANDROID
+        string adUnitId = interstitialId;
+#elif UNITY_IPHONE
+        string adUnitId = "ca-app-pub-3940256099942544/4411468910";
+#else
+        string adUnitId = "unexpected_platform";
+#endif
+
+        // Initialize an InterstitialAd.
+        this.interstitialAd = new InterstitialAd(adUnitId);
+        GameAnalyticsILRD.SubscribeAdMobImpressions(adUnitId, interstitialAd);
+        // Called when an ad request has successfully loaded.
+        this.interstitialAd.OnAdLoaded += HandleOnAdLoaded;
+        // Called when an ad request failed to load.
+        this.interstitialAd.OnAdFailedToLoad += HandleOnAdFailedToLoad;
+        // Called when an ad is shown.
+        this.interstitialAd.OnAdOpening += HandleOnAdOpened;
+        // Called when the ad is closed.
+        this.interstitialAd.OnAdClosed += HandleOnAdClosed;
+        // Called when the ad click caused the user to leave the application.
+//        this.interstitialAd.OnAdLeavingApplication += HandleOnAdLeavingApplication;
+        // Create an empty ad request.
+        AdRequest request = new AdRequest.Builder().Build();
+        // Load the interstitial with the request.
+        this.interstitialAd.LoadAd(request);
     }
 
     private void RequestRewardedAd(){
@@ -112,15 +142,14 @@ public class AdController : MonoBehaviour{
     public void ShowInterstitialAd(){
         if (interstitialAd.IsLoaded()){
             interstitialAd.Show();
-            Firebaseanayltics.current.SetAdImpressionDataAnayltyics();
-        }
-        else{
+        }else{
             RequestInterstitial();
             if (interstitialAd.IsLoaded()){
                 interstitialAd.Show();
             }
             else {
                 Debug.Log("Interstitial is not loaded");
+                GameAnalytics.NewAdEvent(GAAdAction.FailedShow, GAAdType.Interstitial, "admob", interstitialId);
             }
         }
     }
@@ -134,7 +163,7 @@ public class AdController : MonoBehaviour{
             rewardedAd.Show();
             
             GameHandler.i.SetShowingAd(true);
-            Firebaseanayltics.current.SetAdImpressionDataAnayltyics();
+            // Firebaseanayltics.current.SetAdImpressionDataAnayltyics();
         }else{
             RequestRewardedAd();
             if (rewardedAd.IsLoaded()){
@@ -150,9 +179,45 @@ public class AdController : MonoBehaviour{
                 }else if (extraEgg){
                     extraEgg = false;
                 }
+                GameAnalytics.NewAdEvent(GAAdAction.FailedShow, GAAdType.RewardedVideo, "admob", rewardedId);
             }
         }
     }
+
+
+    #region InterstitialBannerHandle
+    public void HandleOnAdLoaded(object sender, EventArgs args){
+        Debug.Log("HandleAdLoaded event received");
+        Debug.Log("args :- " + sender.ToString());
+        // if (sender.Equals("GoogleMobileAds.Api.InterstitialAd")){
+
+        // }
+        if (sender.Equals("GoogleMobileAds.Api.bannerView")){
+            GameAnalytics.NewAdEvent(GAAdAction.Show, GAAdType.Banner, "admob", bannerId);
+        }
+    }
+
+    public void HandleOnAdFailedToLoad(object sender, AdFailedToLoadEventArgs args){
+        Debug.Log("HandleFailedToReceiveAd event received with message: "+ args.ToString());
+    }
+
+    public void HandleOnAdOpened(object sender, EventArgs args){
+        Debug.Log("HandleAdOpened event received");
+        // send ad event
+        GameAnalytics.NewAdEvent(GAAdAction.Clicked, GAAdType.Banner, "admob", bannerId);
+    }
+
+    public void HandleOnAdClosed(object sender, EventArgs args){
+        Debug.Log("HandleAdClosed event received");
+    }
+
+    public void HandleOnAdLeavingApplication(object sender, EventArgs args){
+        Debug.Log("HandleAdLeavingApplication event received");
+        // send ad event - ad click
+        GameAnalytics.NewAdEvent(GAAdAction.Clicked, GAAdType.Interstitial, "admob", interstitialId);
+    }
+
+    #endregion
 
     #region RewardedHandle
 
@@ -180,6 +245,10 @@ public class AdController : MonoBehaviour{
 
     public void HandleRewardedAdOpening(object sender, EventArgs args){
         AudioManager.i.PauseMusic(SoundType.Main_Sound);
+        // keep track of current rewarded video ad
+        currentRewardedVideoPlacement = rewardedId;
+        // start timer for this ad identifier
+        GameAnalytics.StartTimer(currentRewardedVideoPlacement);
     }
 
     public void HandleRewardedAdFailedToShow(object sender, AdErrorEventArgs args){
@@ -203,6 +272,15 @@ public class AdController : MonoBehaviour{
             LevelLoader.instance.MoveToNextLevel();
             levelRewardAd = false;
         }
+
+        if (currentRewardedVideoPlacement != null)
+        {
+            long elapsedTime = GameAnalytics.StopTimer(currentRewardedVideoPlacement);
+            // send ad event for tracking elapsedTime
+            GameAnalytics.NewAdEvent(GAAdAction.Show, GAAdType.RewardedVideo, "admob", rewardedId, elapsedTime);
+            currentRewardedVideoPlacement = null;
+        }
+
     }
 
     private void HandleUserEarnedReward(object sender, Reward args){
@@ -218,7 +296,10 @@ public class AdController : MonoBehaviour{
         // GameHandler.i.GetExtraEgg();
         // Debug.Log("Ads Reward Completed");
         RequestRewardedAd();
-        
+
+        // send ad event - reward recieved
+        GameAnalytics.NewAdEvent(GAAdAction.RewardReceived, GAAdType.RewardedVideo, "admob", rewardedId);
+
     }
     #endregion
 
@@ -234,9 +315,22 @@ public class AdController : MonoBehaviour{
             string adUnitId = "unexpected_platform";
 #endif
 
+
+
             // Create a 320x50 banner at the top of the screen.
             this.bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Bottom);
+            GameAnalyticsILRD.SubscribeAdMobImpressions(adUnitId, bannerView);
 
+            // Called when an ad request has successfully loaded.
+            this.bannerView.OnAdLoaded += this.HandleOnAdLoaded;
+            // Called when an ad request failed to load.
+            this.bannerView.OnAdFailedToLoad += this.HandleOnAdFailedToLoad;
+            // Called when an ad is clicked.
+            this.bannerView.OnAdOpening += this.HandleOnAdOpened;
+            // Called when the user returned from the app after an ad click.
+            this.bannerView.OnAdClosed += this.HandleOnAdClosed;
+            // Called when the ad click caused the user to leave the application.
+//            this.bannerView.OnAdLeavingApplication += this.HandleOnAdLeavingApplication;
             // Create an empty ad request.
             AdRequest request = new AdRequest.Builder().Build();
 
@@ -252,7 +346,39 @@ public class AdController : MonoBehaviour{
     }
 
     #endregion
-    
-    
-    
+
+    #region GameAnalytics
+
+    public void GameAnalyticsATTListenerNotDetermined(){
+        GameAnalytics.Initialize();
+    }
+    public void GameAnalyticsATTListenerRestricted(){
+        GameAnalytics.Initialize();
+    }
+    public void GameAnalyticsATTListenerDenied(){
+        GameAnalytics.Initialize();
+    }
+    public void GameAnalyticsATTListenerAuthorized(){
+        GameAnalytics.Initialize();
+    }
+
+    #endregion
+
+    // when application goes to background (during the display of a rewarded video ad) then the timer needs to stop.
+    // therefore we need to call code in Unity method OnApplicationPause.
+    void OnApplicationPause(bool paused){
+        if (paused){
+            if (currentRewardedVideoPlacement != null){
+                GameAnalytics.PauseTimer(currentRewardedVideoPlacement);
+            }
+        }else{
+            if (currentRewardedVideoPlacement != null){
+                GameAnalytics.ResumeTimer(currentRewardedVideoPlacement);
+            }
+        }
+
+    }
+
+
+
 }
